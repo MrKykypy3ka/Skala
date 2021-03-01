49a0d91d2d25b4bf503e39817b3c896066453773
MrKykypy3ka <79640118+MrKykypy3ka@users.noreply.github.com>
2021-03-01 20:18:53 +0900

    Initial commit

---
+++ Skala/Mounts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Skala
+{
+    public class Mounts
+    {
+        int[] markup, markup1;
+        string[,] mount;
+        Random rand = new Random();
+        int x, y;
+        public Mounts(int x, int y, int m)
+        {
+            markup = new int[x];
+            mount = new String[x, y];
+            markup[0] = rand.Next(0, y);
+            this.x = x;
+            this.y = y;
+            for (int i = 1; i < x; i++)
+            {
+                do
+                    markup[i] = rand.Next(markup[i - 1] - m, markup[i - 1] + m + 1);
+                while (markup[i] > y || markup[i] < 0);
+            }
+            markup1 = (int[])markup.Clone();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = y - 1; j >= 0; j--)
+                {
+                    if (j == y - 1 && markup1[i] == 0)
+                        mount[i, j] = "—";
+                    else if (markup1[i] > 0)
+                    {
+                        mount[i, j] = "*";
+                        markup1[i]--;
+                    }
+                    else
+                        mount[i, j] = " ";
+                }
+            }
+        }
+        public void print()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < x; i++)
+                Console.Write(markup[i] + " ");
+        }
+        public void draw()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                    Console.Write(mount[j, i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Skala/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Skala
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int x = 0, y = 0, m = 0;
+            Console.WriteLine("Введите максимальную высоту");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите продолжительность пути");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальный перепад высот смежных скал");
+            m = Convert.ToInt32(Console.ReadLine());
+            Mounts one = new Mounts(x, y, m);
+            one.print();
+            one.draw();
+            Console.ReadKey(true);
+        }
+    }
+}
